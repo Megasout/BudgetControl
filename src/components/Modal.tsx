a8379b37a234edf50287f4a6570bf0236d495baa
Megasout <Megasoutt@gmail.com>
2023-09-01 21:46:59 -0300

    new bills form created with animation
    se creo el formulario de nuevo gasto y se le agrego una animación al abrirlo

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import "./css/Modal.css"
 import CloseIcon from "../assets/cerrar.svg"
+import { useEffect, useState } from "react"
 
 type ModalProps = {
     setModal(value: boolean): void
@@ -12,6 +13,18 @@ function Modal(props: ModalProps) {
         setModal(false)
     }
 
+    const [animation, setAnimation] = useState<boolean>(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('Animando')
+            setAnimation(true)
+        }
+        , 500)
+    }, [])
+
+    const formClass = (animation) ? 'modal-form-animation' : ''
+
     return (
         <div id="modal">
             <img
@@ -19,22 +32,30 @@ function Modal(props: ModalProps) {
                 src={CloseIcon}
                 alt="Icono de Cierre"
                 onClick={handleCloseButton} />
-            <form id="modal-form">
+            <form className={"modal-form" + ' ' + formClass}>
                 <legend id="modal-title">Nuevo Gasto</legend>
                 <label className="modal-label">Nombre del Gasto</label>
-                <input className="modal-input" type="text"/>
-                <label className="modal-input">Cantidad</label>
-                <input className="modal-input" type="text"/>
-                <label className="modal-input">Filtrar Gasto</label>
-                <select name="Seleccionar">
-                    <option>Seleccionar</option>
-                    <option value={"Suscripiones"}>Suscripiones</option>
-                    <option value={"Ocio"}>Ocio</option>
-                    <option value={"Hogar"}>Hogar</option>
-                    <option value={"Salud"}>Salud</option>
+                <input
+                    className="modal-input"
+                    type="text"
+                    placeholder="Añade el Nombre del Gasto" />
+                <label className="modal-label">Cantidad</label>
+                <input
+                    className="modal-input"
+                    type="number"
+                    placeholder="Añade la Cantidad del Gasto: ej.300" />
+                <label className="modal-label">Filtrar Gasto</label>
+                <select id="modal-select">
+                    <option>--Seleccionar--</option>
+                    <option value={"Ahorro"}>Comida</option>
                     <option value={"Comida"}>Comida</option>
+                    <option value={"Hogar"}>Hogar</option>
+                    <option value={"Otros Gastos"}>Comida</option>
+                    <option value={"Ocio"}>Ocio</option>
+                    <option value={"Salud"}>Salud</option>
+                    <option value={"Suscripiones"}>Suscripiones</option>
                 </select>
-                <input type="submit"/>
+                <input id="modal-submit" type="submit" />
             </form>
         </div>
     )
