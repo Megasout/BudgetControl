3d36e915454de88a1d69bb188d43dd08a777fd49
Megasout <Megasoutt@gmail.com>
2023-09-03 20:59:18 -0300

    update bill component
    se actualizó el re size del bill component

--- src/components/BillsComponents/Bill.tsx
+++ src/components/BillsComponents/Bill.tsx
@@ -12,14 +12,16 @@ function Bill(props: BillProps) {
     const { icon, amount, date, name, type } = props
 
     return (
-        <div id="billsContainer">
-            <img className="icon" src={icon} />
-            <div style={{marginLeft: '20px'}}>
-                <p className="normalLabel">{type}</p>
-                <p className="nameLabel">{name}</p>
-                <p>Agregado el:
-                    <span className="normalLabel"> {date}</span>
-                </p>
+        <div id="billContainer">
+            <div className="billData">
+                <img className="icon" src={icon} />
+                <div style={{ marginLeft: '20px' }}>
+                    <p className="normalLabel">{type}</p>
+                    <p className="nameLabel">{name}</p>
+                    <p>Agregado el:
+                        <span className="normalLabel"> {date}</span>
+                    </p>
+                </div>
             </div>
             <h2 className="amount">{amount}</h2>
         </div>
