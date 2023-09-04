fb1aceec8f5872a19f98b3b76a2130211614f0e8
Megasout <Megasoutt@gmail.com>
2023-09-04 16:11:09 -0300

    install prop-types

--- src/components/BillsComponents/Bill.tsx
+++ src/components/BillsComponents/Bill.tsx
@@ -1,4 +1,12 @@
 import "./css/Bill.css"
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css'
 
 type BillProps = {
     icon: string
@@ -12,19 +20,21 @@ function Bill(props: BillProps) {
     const { icon, amount, date, name, type } = props
 
     return (
-        <div id="billContainer">
-            <div className="billData">
-                <img className="icon" src={icon} />
-                <div style={{ marginLeft: '20px', paddingTop:'10px' }}>
-                    <p className="normalLabel">{type}</p>
-                    <p className="nameLabel">{name}</p>
-                    <p>Agregado el:
-                        <span className="normalLabel"> {date}</span>
-                    </p>
+        <SwipeableList>
+            <div id="billContainer">
+                <div className="billData">
+                    <img className="icon" src={icon} />
+                    <div style={{ marginLeft: '20px', paddingTop: '10px' }}>
+                        <p className="normalLabel">{type}</p>
+                        <p className="nameLabel">{name}</p>
+                        <p>Agregado el:
+                            <span className="normalLabel"> {date}</span>
+                        </p>
+                    </div>
                 </div>
+                <h2 className="amount">{amount}</h2>
             </div>
-            <h2 className="amount">{amount}</h2>
-        </div>
+        </SwipeableList>
     )
 }
 
