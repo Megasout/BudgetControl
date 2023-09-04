7677c0a1ce017060692d0f878d8c3ec1ab19bcba
Megasout <Megasoutt@gmail.com>
2023-09-04 17:55:54 -0300

    style Added to swipeable list

--- src/components/BillsComponents/Bill.tsx
+++ src/components/BillsComponents/Bill.tsx
@@ -8,8 +8,6 @@ import {
 } from 'react-swipeable-list'
 import 'react-swipeable-list/dist/styles.css';
 
-import { ReactNode } from 'react'
-
 type BillProps = {
     icon: string
     name: string
@@ -24,40 +22,47 @@ function Bill(props: BillProps) {
     const leadingActions = () => (
         <LeadingActions>
             <SwipeAction onClick={() => console.log('editar')}>
-                Editar
+                <div>
+                    <h3>Editar</h3>
+                </div>
             </SwipeAction>
         </LeadingActions>
     )
 
     const trailingActions = () => (
         <TrailingActions>
-            <SwipeAction destructive={true} onClick={() => console.log('eliminar')}>
-                Eliminar
+            <SwipeAction destructive={false} onClick={() => console.log('eliminar')}>
+                <div>
+                    <h3>Eliminar</h3>
+                </div>
             </SwipeAction>
         </TrailingActions>
     )
 
     return (
-        <SwipeableList>
-            <SwipeableListItem
-                leadingActions={leadingActions()}
-                trailingActions={trailingActions()}
-                maxSwipe={1.0}>
-                <div id="billContainer">
-                    <div className="billData">
-                        <img className="icon" src={icon} />
-                        <div style={{ marginLeft: '20px', paddingTop: '10px' }}>
-                            <p className="normalLabel">{type}</p>
-                            <p className="nameLabel">{name}</p>
-                            <p>Agregado el:
-                                <span className="normalLabel"> {date}</span>
-                            </p>
+        <div id="billContainerData">
+
+            <SwipeableList>
+                <SwipeableListItem
+                    leadingActions={leadingActions()}
+                    trailingActions={trailingActions()}
+                    maxSwipe={1.0}>
+                    <div id="billContainer">
+                        <div className="billData">
+                            <img className="icon" src={icon} draggable={false} />
+                            <div style={{ marginLeft: '20px', paddingTop: '10px' }}>
+                                <p className="normalLabel">{type}</p>
+                                <p className="nameLabel">{name}</p>
+                                <p>Agregado el:
+                                    <span className="normalLabel"> {date}</span>
+                                </p>
+                            </div>
                         </div>
+                        <h2 className="amount">{amount}</h2>
                     </div>
-                    <h2 className="amount">{amount}</h2>
-                </div>
-             </SwipeableListItem>
-         </SwipeableList>
+                </SwipeableListItem>
+            </SwipeableList>
+        </div>
     )
 }
 
