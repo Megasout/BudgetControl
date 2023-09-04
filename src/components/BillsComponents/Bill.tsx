bfb7a8268179ccb4e24344945f11a6e6d0bb803d
Megasout <Megasoutt@gmail.com>
2023-09-04 16:46:29 -0300

    Update Bill.tsx
    components SwipeableListItem and SwipeableList added

--- src/components/BillsComponents/Bill.tsx
+++ src/components/BillsComponents/Bill.tsx
@@ -6,7 +6,9 @@ import {
     SwipeAction,
     TrailingActions
 } from 'react-swipeable-list'
-import 'react-swipeable-list/dist/styles.css'
+import 'react-swipeable-list/dist/styles.css';
+
+import { ReactNode } from 'react'
 
 type BillProps = {
     icon: string
@@ -19,22 +21,43 @@ type BillProps = {
 function Bill(props: BillProps) {
     const { icon, amount, date, name, type } = props
 
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => console.log('editar')}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction destructive={true} onClick={() => console.log('eliminar')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
+
     return (
         <SwipeableList>
-            <div id="billContainer">
-                <div className="billData">
-                    <img className="icon" src={icon} />
-                    <div style={{ marginLeft: '20px', paddingTop: '10px' }}>
-                        <p className="normalLabel">{type}</p>
-                        <p className="nameLabel">{name}</p>
-                        <p>Agregado el:
-                            <span className="normalLabel"> {date}</span>
-                        </p>
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+                maxSwipe={1.0}>
+                <div id="billContainer">
+                    <div className="billData">
+                        <img className="icon" src={icon} />
+                        <div style={{ marginLeft: '20px', paddingTop: '10px' }}>
+                            <p className="normalLabel">{type}</p>
+                            <p className="nameLabel">{name}</p>
+                            <p>Agregado el:
+                                <span className="normalLabel"> {date}</span>
+                            </p>
+                        </div>
                     </div>
+                    <h2 className="amount">{amount}</h2>
                 </div>
-                <h2 className="amount">{amount}</h2>
-            </div>
-        </SwipeableList>
+             </SwipeableListItem>
+         </SwipeableList>
     )
 }
 
